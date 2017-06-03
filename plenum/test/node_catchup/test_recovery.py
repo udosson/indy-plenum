48b8f7c90a81e49600eee06f70b81c576cfa1534
Lovesh <lovesh.harchandani@evernym.com>
2017-06-04 00:18:38 +0530

    commenting code to restart the node and catchup

--- plenum/test/node_catchup/test_recovery.py
+++ plenum/test/node_catchup/test_recovery.py
@@ -1,5 +1,6 @@
 from time import perf_counter
 
+import logging
 import pytest
 
 from plenum.common.constants import DOMAIN_LEDGER_ID, LedgerState
@@ -10,7 +11,7 @@ from stp_zmq.zstack import KITZStack
 
 from stp_core.loop.eventually import eventually
 from plenum.common.types import HA
-from stp_core.common.log import getlogger
+from stp_core.common.log import getlogger, Logger
 from plenum.test.helper import sendReqsToNodesAndVerifySuffReplies
 from plenum.test.node_catchup.helper import waitNodeDataEquality, \
     check_ledger_state
@@ -23,9 +24,11 @@ from plenum.test import waits
 # Do not remove the next import
 from plenum.test.node_catchup.conftest import whitelist
 
+Logger.setLogLevel(logging.INFO)
+
+
 logger = getlogger()
 txnCount = 5
-
 
 @pytest.fixture(scope="function", autouse=True)
 def limitTestRunningTime():
@@ -64,43 +67,43 @@ def testNodeCatchupAfterRestart(newNodeCaughtUp, txnPoolNodeSet, tconf,
     looper, newNode, client, wallet, _, _ = nodeSetWithNodeAddedAfterSomeTxns
     logger.debug("Stopping node {} with pool ledger size {}".
                  format(newNode, newNode.poolManager.txnSeqNo))
-    disconnect_node_and_ensure_disconnected(looper, txnPoolNodeSet, newNode)
-    looper.removeProdable(newNode)
+    # disconnect_node_and_ensure_disconnected(looper, txnPoolNodeSet, newNode)
+    # looper.removeProdable(newNode)
     # TODO: Check if the node has really stopped processing requests?
     logger.debug("Sending requests")
 
     # Here's where we apply some load
-    for i in range(100):
+    for i in range(150):
         # c, w = buildPoolClientAndWallet(poolTxnStewardData,
         #                                           tdirWithPoolTxns,
         #                                           clientClass=TestClient)
         # looper.add(c)
         # sendReqsToNodesAndVerifySuffReplies(looper, w, c, 5)
-        sendReqsToNodesAndVerifySuffReplies(looper, wallet, client, 5)
+        sendReqsToNodesAndVerifySuffReplies(looper, wallet, client, 10)
         # looper.removeProdable(c)
 
     logger.debug("Starting the stopped node, {}".format(newNode))
     nodeHa, nodeCHa = HA(*newNode.nodestack.ha), HA(*newNode.clientstack.ha)
-    newNode = TestNode(newNode.name, basedirpath=tdirWithPoolTxns, config=tconf,
-                       ha=nodeHa, cliha=nodeCHa, pluginPaths=allPluginsPath)
-    looper.add(newNode)
-    txnPoolNodeSet[-1] = newNode
+    # newNode = TestNode(newNode.name, basedirpath=tdirWithPoolTxns, config=tconf,
+    #                    ha=nodeHa, cliha=nodeCHa, pluginPaths=allPluginsPath)
+    # looper.add(newNode)
+    # txnPoolNodeSet[-1] = newNode
 
     # Delay catchup reply processing so LedgerState does not change
-    delay_catchup_reply = 5
-    newNode.nodeIbStasher.delay(cr_delay(delay_catchup_reply))
-    looper.run(checkNodesConnected(txnPoolNodeSet))
-
-    # Make sure ledger starts syncing (sufficient consistency proofs received)
-    looper.run(eventually(check_ledger_state, newNode, DOMAIN_LEDGER_ID,
-                          LedgerState.syncing, retryWait=.5, timeout=5))
-
-    # Not accurate timeout but a conservative one
-    timeout = waits.expectedPoolGetReadyTimeout(len(txnPoolNodeSet)) + \
-              2*delay_catchup_reply
-    waitNodeDataEquality(looper, newNode, *txnPoolNodeSet[:4],
-                         customTimeout=timeout)
-
+    # delay_catchup_reply = 5
+    # newNode.nodeIbStasher.delay(cr_delay(delay_catchup_reply))
+    # looper.run(checkNodesConnected(txnPoolNodeSet))
+    #
+    # # Make sure ledger starts syncing (sufficient consistency proofs received)
+    # looper.run(eventually(check_ledger_state, newNode, DOMAIN_LEDGER_ID,
+    #                       LedgerState.syncing, retryWait=.5, timeout=5))
+    #
+    # # Not accurate timeout but a conservative one
+    # timeout = waits.expectedPoolGetReadyTimeout(len(txnPoolNodeSet)) + \
+    #           2*delay_catchup_reply
+    # waitNodeDataEquality(looper, newNode, *txnPoolNodeSet[:4],
+    #                      customTimeout=timeout)
+    #
     sendReqsToNodesAndVerifySuffReplies(looper, wallet, client, 5)
     waitNodeDataEquality(looper, newNode, *txnPoolNodeSet[:4], customTimeout=timeout)
 
